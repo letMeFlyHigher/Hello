a0f09001418ca1f0a9d71717df5dbd1feba80c98
Robot F <996245864@qq.com>
2017-12-28 00:13:37 +0800

    谁知道这是啥，请告诉我///

---
+++ HelloFriend/src/main/java/cn/itcast/maven/HelloFriend.java
@@ -0,0 +1,8 @@
+package cn.itcast.maven;
+
+public class HelloFriend {
+	public void helloFriend(){
+		Hello hello = new Hello();
+		hello.hello();
+	}
+}
---
+++ HelloFriend/src/test/java/cn/itcast/maven/HelloFriendTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.maven;
+
+import org.junit.Test;
+
+public class HelloFriendTest {
+
+	@Test
+	public void testHelloFriend(){
+		HelloFriend helloFriend = new HelloFriend();
+		helloFriend.helloFriend();
+	}
+}
